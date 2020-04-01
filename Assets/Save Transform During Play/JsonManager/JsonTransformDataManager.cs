60d08c983ff17824eabd24781b4366126fdebf7e
rito <ghdtkals15@naver.com>
2020-04-02 03:54:05 +0900

    [2020-04-02] Save Transform 설명 추가

--- Assets/Save Transform During Play/JsonManager/JsonTransformDataManager.cs
+++ Assets/Save Transform During Play/JsonManager/JsonTransformDataManager.cs
@@ -2,68 +2,71 @@
 using System.IO;
 using System;
 
-public static class JsonTransformDataManager
+namespace Rito.Conveniences
 {
-    private static string FolderPath => Application.dataPath + "/Save Transform During Play/Data/";
-    private static string GetFullPath(int id) => FolderPath + $"{id}.json";
-
-    public static void SaveTransformDataToJSON(in TransformData data, int id)
+    public static class JsonTransformDataManager
     {
-        DirectoryInfo di = new DirectoryInfo(FolderPath);
-        if (di.Exists == false) di.Create();
+        private static string FolderPath => Application.dataPath + "/Save Transform During Play/Data/";
+        private static string GetFullPath(int id) => FolderPath + $"{id}.json";
 
-        File.WriteAllText(GetFullPath(id), JsonUtility.ToJson(data));
-    }
-
-    public static TransformData LoadTransformDataFromJSON(int id)
-    {
-        string filePath = GetFullPath(id);
-
-        // 파일 미존재 예외처리
-        if (File.Exists(filePath) == false)
-            return TransformData.Null;
-
-        // 정상 로드
-        string jsonStr = File.ReadAllText(filePath);
-        TransformData data = JsonUtility.FromJson<TransformData>(jsonStr);
-        return data;
-    }
-}
-
-[Serializable]
-public struct TransformData
-{
-    public Vector3    localPosition;
-    public Quaternion localRotation;
-    public Vector3    localScale;
-    public bool       isApplied;
-
-    // 생성자
-    public TransformData(in Transform transform)
-    {
-        localPosition = transform.localPosition;
-        localRotation = transform.localRotation;
-        localScale = transform.localScale;
-
-        isApplied = false;
-    }
-
-    /// <summary> Struct -> Transform </summary>
-    public void Load(in Transform transform)
-    {
-        transform.localPosition = localPosition;
-        transform.localRotation = localRotation;
-        transform.localScale = localScale;
-    }
-
-    // null 패턴
-    private static TransformData _null
-        = new TransformData
+        public static void SaveTransformDataToJSON(in TransformData data, int id)
         {
-            localPosition = Vector3.zero,
-            localRotation = Quaternion.identity,
-            localScale = Vector3.one,
-            isApplied = false
-        };
-    public static TransformData Null => _null;
+            DirectoryInfo di = new DirectoryInfo(FolderPath);
+            if (di.Exists == false) di.Create();
+
+            File.WriteAllText(GetFullPath(id), JsonUtility.ToJson(data));
+        }
+
+        public static TransformData LoadTransformDataFromJSON(int id)
+        {
+            string filePath = GetFullPath(id);
+
+            // 파일 미존재 예외처리
+            if (File.Exists(filePath) == false)
+                return TransformData.Null;
+
+            // 정상 로드
+            string jsonStr = File.ReadAllText(filePath);
+            TransformData data = JsonUtility.FromJson<TransformData>(jsonStr);
+            return data;
+        }
+    }
+
+    [Serializable]
+    public struct TransformData
+    {
+        public Vector3 localPosition;
+        public Quaternion localRotation;
+        public Vector3 localScale;
+        public bool isApplied;
+
+        // 생성자
+        public TransformData(in Transform transform)
+        {
+            localPosition = transform.localPosition;
+            localRotation = transform.localRotation;
+            localScale = transform.localScale;
+
+            isApplied = false;
+        }
+
+        /// <summary> Struct -> Transform </summary>
+        public void Load(in Transform transform)
+        {
+            transform.localPosition = localPosition;
+            transform.localRotation = localRotation;
+            transform.localScale = localScale;
+        }
+
+        // null 패턴
+        private static TransformData _null
+            = new TransformData
+            {
+                localPosition = Vector3.zero,
+                localRotation = Quaternion.identity,
+                localScale = Vector3.one,
+                isApplied = false
+            };
+        public static TransformData Null => _null;
+    }
 }
--- Assets/Save Transform During Play/SaveTransformDuringPlay.cs
+++ Assets/Save Transform During Play/SaveTransformDuringPlay.cs
@@ -2,117 +2,126 @@
 
 using UnityEngine;
 
-[ExecuteInEditMode]
-public class SaveTransformDuringPlay : MonoBehaviour
+// 2020. 04. 01. 작성
+// 제작자 : Rito
+// 기능   : 플레이 모드에서 Transform을 변경한 경우, 플레이 모드가 종료되어도 변경사항을 적용할 수 있게 합니다.
+// 사용법 : 원하는 게임오브젝트에 SaveTransformDuringPlay 컴포넌트를 추가합니다.
+//          인스펙터에서 On 변수를 체크/해제함으로써 언제든 기능을 켜고 끌 수 있습니다.
+
+namespace Rito.Conveniences
 {
-    /// <summary> 기능 On/Off </summary>
-    public bool _on = true;
-
-    private const int Yes = 1;
-    private const int No = 0;
-
-    private bool _onEditorMode = false;
-
-    private void OnGUI()
+    [ExecuteInEditMode]
+    public class SaveTransformDuringPlay : MonoBehaviour
     {
-        if (!Application.isPlaying)
+        /// <summary> 기능 On/Off </summary>
+        public bool _on = true;
+
+        private const int Yes = 1;
+        private const int No = 0;
+
+        private bool _onEditorMode = false;
+
+        private void OnGUI()
         {
+            if (!Application.isPlaying)
+            {
 #if USE_DEBUG
             Debug.Log("Method Call : OnGUI(), Editor Mode");
 #endif
-            if (_onEditorMode == false)
-            {
-                OnEditorMode();
-                _onEditorMode = true;
+                if (_onEditorMode == false)
+                {
+                    OnEditorMode();
+                    _onEditorMode = true;
+                }
+
+                // 플레이모드가 종료되고, 에디터모드가 시작된 순간에 트랜스폼 변경사항 적용
+                if (_on && CheckPrefs(nameof(Prefs._tmApplied), No))
+                    ApplyModifications();
             }
-
-            // 플레이모드가 종료되고, 에디터모드가 시작된 순간에 트랜스폼 변경사항 적용
-            if (_on && CheckPrefs(nameof(Prefs._tmApplied), No))
-                ApplyModifications();
         }
-    }
 
-    // 플레이모드가 종료되는 순간 JSON으로 저장
-    private void OnApplicationQuit()
-    {
+        // 플레이모드가 종료되는 순간 JSON으로 저장
+        private void OnApplicationQuit()
+        {
 #if USE_DEBUG
         Debug.Log("Method Call : OnApplicationQuit()");
 #endif
-        if (_on)
-        {
-            JsonTransformDataManager.SaveTransformDataToJSON(new TransformData(transform), GetInstanceID());
-            SavePrefs(nameof(Prefs._tmApplied), No);
+            if (_on)
+            {
+                JsonTransformDataManager.SaveTransformDataToJSON(new TransformData(transform), GetInstanceID());
+                SavePrefs(nameof(Prefs._tmApplied), No);
+            }
+
+            SavePrefs(nameof(Prefs._tmaOn), _on ? Yes : No);
         }
 
-        SavePrefs(nameof(Prefs._tmaOn), _on ? Yes : No);
-    }
-
-    // 에디터모드가 실행되는 순간 호출
-    private void OnEditorMode()
-    {
+        // 에디터모드가 실행되는 순간 호출
+        private void OnEditorMode()
+        {
 #if USE_DEBUG
         Debug.Log("Method Call : OnEditorMode()");
 #endif
-        // 플레이 도중 _on : false -> true로 바꾼 경우
-        if (!_on && CheckPrefs(nameof(Prefs._tmaOn), Yes))
-        {
-            _on = true;
+            // 플레이 도중 _on : false -> true로 바꾼 경우
+            if (!_on && CheckPrefs(nameof(Prefs._tmaOn), Yes))
+            {
+                _on = true;
 
 #if USE_DEBUG
             Debug.Log("플레이 도중 기능 ON");
 #endif
-        }
+            }
 
-        // 플레이 도중 _on : true -> false 바꾼 경우
-        else if (_on && CheckPrefs(nameof(Prefs._tmaOn), No))
-        {
-            _on = false;
+            // 플레이 도중 _on : true -> false 바꾼 경우
+            else if (_on && CheckPrefs(nameof(Prefs._tmaOn), No))
+            {
+                _on = false;
 
 #if USE_DEBUG
             Debug.Log("플레이 도중 기능 OFF");
 #endif
+            }
         }
-    }
 
-    private void ApplyModifications()
-    {
+        private void ApplyModifications()
+        {
 #if USE_DEBUG
         Debug.Log("Method Call : ApplyModifications()");
 #endif
-        TransformData savedData = JsonTransformDataManager.LoadTransformDataFromJSON(GetInstanceID());
+            TransformData savedData = JsonTransformDataManager.LoadTransformDataFromJSON(GetInstanceID());
 
-        if (savedData.Equals(TransformData.Null) || savedData.isApplied || !enabled || !_on)
-            return;
+            if (savedData.Equals(TransformData.Null) || savedData.isApplied || !enabled || !_on)
+                return;
 
-        savedData.Load(transform);
-        SavePrefs(nameof(Prefs._tmApplied), Yes);
+            savedData.Load(transform);
+            SavePrefs(nameof(Prefs._tmApplied), Yes);
 
-        savedData.isApplied = true;
-        JsonTransformDataManager.SaveTransformDataToJSON(savedData, GetInstanceID());
-    }
+            savedData.isApplied = true;
+            JsonTransformDataManager.SaveTransformDataToJSON(savedData, GetInstanceID());
+        }
 
 
-    private void SavePrefs(in string prefName, in int yesOrNo)
-    {
+        private void SavePrefs(in string prefName, in int yesOrNo)
+        {
 #if USE_DEBUG
         Debug.Log($"Method Call : SavePrefs({prefName})");
 #endif
-        PlayerPrefs.SetInt(GetInstanceID() + prefName, yesOrNo);
-    }
+            PlayerPrefs.SetInt(GetInstanceID() + prefName, yesOrNo);
+        }
 
-    private bool CheckPrefs(in string prefName, int yesOrNo)
-    {
+        private bool CheckPrefs(in string prefName, int yesOrNo)
+        {
 #if USE_DEBUG
         Debug.Log($"Method Call : CheckPrefs({prefName})");
 #endif
-        return PlayerPrefs.GetInt(GetInstanceID() + prefName).Equals(yesOrNo);
-    }
+            return PlayerPrefs.GetInt(GetInstanceID() + prefName).Equals(yesOrNo);
+        }
 
-    private enum Prefs
-    {
-        /// <summary> Transform 변경사항이 성공적으로 적용됨 </summary>
-        _tmApplied,
-        /// <summary> 기능 사용 여부 </summary>
-        _tmaOn
+        private enum Prefs
+        {
+            /// <summary> Transform 변경사항이 성공적으로 적용됨 </summary>
+            _tmApplied,
+            /// <summary> 기능 사용 여부 </summary>
+            _tmaOn
+        }
     }
 }
